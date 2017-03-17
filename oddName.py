0b09c21cb7190f60cce3f43d6f7fe6fe54468bb6
jc432592 <graham.ruttiman@my.jcu.edu.au>
2017-03-17 15:22:39 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,2 +1,10 @@
-""""""GRAHAM RUTTIMAN""""""
+"""GRAHAM RUTTIMAN"""
+name = input("What is your name?")
+if name=="":
+    name = input("Please tell me your name?")
+else:
+    for i in range (1,len(name),2):
+        print(name[i])
+
+
 
