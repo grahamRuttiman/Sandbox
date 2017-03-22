c3328a80bb868ac5481b08cf2b6254a89e4b5488
jc432592 <graham.ruttiman@my.jcu.edu.au>
2017-03-22 17:05:25 +1000

    copy 1st version to git

--- oddName.py
+++ oddName.py
@@ -1,11 +1,9 @@
 """GRAHAM RUTTIMAN"""
-name = input("What is your name?")
-if name=="":
-    name = input("Please tell me your name?")
-else:
-    for i in range (1,len(name),2):
-        print(name[i])
-
-
-
-
+def main()
+    name = input("What is your name?")
+    if name=="":
+        name = input("Please tell me your name?")
+    else:
+        for i in range (1,len(name),2):
+            print(name[i])
+main()
